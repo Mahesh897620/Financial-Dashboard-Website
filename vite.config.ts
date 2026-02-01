9eb95935464c0bf4f444a2326f1534af9895a03a
Mahesh897620 <thatiparthimahesh31@gmail.com>
2026-02-01 17:32:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-  base: "/",
+  base: "/Financial-Dashboard/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
